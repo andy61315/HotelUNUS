e572738e0cc50513d739c2bd5f0a037f91c962ea
andy61315 <54438122+andy61315@users.noreply.github.com>
2020-05-18 17:56:45 +0800

    Update MailService.java

--- src/common/MailService.java
+++ src/common/MailService.java
@@ -26,8 +26,8 @@ public class MailService {
 
 	       // ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
 	       // ●須將myGmail的【安全性較低的應用程式存取權】打開
-		     final String myGmail = "kancolle86web@gmail.com";
-		     final String myGmail_password = "x45635420";
+		     final String myGmail = "kale@gmail.com";
+		     final String myGmail_password = "x20";
 			   Session session = Session.getInstance(props, new Authenticator() {
 				   protected PasswordAuthentication getPasswordAuthentication() {
 					   return new PasswordAuthentication(myGmail, myGmail_password);
